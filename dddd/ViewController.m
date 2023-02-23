b61c1d40bd76cad101b4b0c4ef33e14b1df654fc
huangyuandi <413838603@qq.com>
2023-02-23 17:19:56 +0800

    Update ViewController.m

--- dddd/ViewController.m
+++ dddd/ViewController.m
@@ -16,7 +16,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [TestLog log];
-    //高效啊使用git. 
+    //高效啊使用git. 阿达达法师发发呆分
     //1231款3金龙坑就哭了23就离开家
     // Do any additional setup after loading the view.
 }
