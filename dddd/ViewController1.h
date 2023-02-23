bf54b9f2d67b5bea1f8fbf3267f4fc48f684b620
onesoft <413838603@qq.com>
2023-02-23 18:25:52 +0800

    dad

---
+++ dddd/ViewController1.h
@@ -0,0 +1,16 @@
+//
+//  ViewController1.h
+//  dddd
+//
+//  Created by onesoft on 2023/2/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController1 : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ dddd/ViewController1.m
@@ -0,0 +1,31 @@
+//
+//  ViewController1.m
+//  dddd
+//
+//  Created by onesoft on 2023/2/23.
+//
+
+#import "ViewController1.h"
+
+@interface ViewController1 ()
+
+@end
+
+@implementation ViewController1
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
